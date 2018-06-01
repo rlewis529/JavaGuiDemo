73b8d752687df1b37dc435f27afa24c2ed2976d0
Robert Lewis <rlewis@relias.int>
2018-06-01 16:39:17 -0400

    more friday 6-1 commits

--- src/Frame1.java
+++ src/Frame1.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JOptionPane;
 
 public class Frame1 {
 
@@ -44,9 +45,15 @@ public class Frame1 {
 		JButton btnNewButton = new JButton("Help");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				//infoBox("test message", "test title bar"); //This works but trying something different...
+				JOptionPane.showMessageDialog(null, "ALERT MESSAGE WOOHOO", "TITLE WOOHOO", JOptionPane.WARNING_MESSAGE);
 			}
 		});
 		btnNewButton.setBounds(189, 129, 154, 48);
 		frame.getContentPane().add(btnNewButton);
+	}
+	
+	public static void infoBox(String infoMessage, String titleBar) {
+		JOptionPane.showMessageDialog(null, infoMessage, "InfoBox: " + titleBar, JOptionPane.INFORMATION_MESSAGE);
 	}
 }
